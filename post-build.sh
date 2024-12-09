8941e145d89962526a3ddfbcd2cf88f26afc53cf
Classic1234 <45549738+Classic1234@users.noreply.github.com>
2024-12-09 17:04:08 +0100

    Update post-build.sh

--- post-build.sh
+++ post-build.sh
@@ -1,6 +1,6 @@
-sudo wget https://raw.githubusercontent.com/mitchamador/rt-n56u/master/rt-n56u-mt7621_cpufreq.sh
-sudo wget https://raw.githubusercontent.com/mitchamador/rt-n56u/master/rt-n56u-mt7621_set_cpufreq.sh
-sudo chmod +x rt-n56u-mt7621_cpufreq.sh
-sudo chmod +x rt-n56u-mt7621_set_cpufreq.sh
-sudo sh rt-n56u-mt7621_cpufreq.sh
-sudo sh rt-n56u-mt7621_set_cpufreq.sh 1100
+wget https://raw.githubusercontent.com/mitchamador/rt-n56u/master/rt-n56u-mt7621_cpufreq.sh
+wget https://raw.githubusercontent.com/mitchamador/rt-n56u/master/rt-n56u-mt7621_set_cpufreq.sh
+chmod +x rt-n56u-mt7621_cpufreq.sh
+chmod +x rt-n56u-mt7621_set_cpufreq.sh
+sh rt-n56u-mt7621_cpufreq.sh
+sh rt-n56u-mt7621_set_cpufreq.sh 1100
