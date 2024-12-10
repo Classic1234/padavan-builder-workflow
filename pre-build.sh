6da81b77f6de16be21e9788955b123804e9a7efe
Classic1234 <45549738+Classic1234@users.noreply.github.com>
2024-12-10 01:55:15 +0100

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -6,3 +6,6 @@ sh rt-n56u-mt7621_cpufreq.sh
 sh rt-n56u-mt7621_set_cpufreq.sh 1100
 /clear_tree
 ./build_firmware
+
+cd /opt/rt-n56u
+wget -q https://raw.githubusercontent.com/mitchamador/rt-n56u/master/rt-n56u-mt7621_set_cpufreq.sh -O - | bash -s -- 1100
