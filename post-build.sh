2aace20c31b3077b06524751776053cf82fb895a
Classic1234 <45549738+Classic1234@users.noreply.github.com>
2024-12-12 03:12:39 +0100

    Update and rename prbuild.sh to post-build.sh

---
+++ post-build.sh
@@ -0,0 +1,7 @@
+cd rt-n56u
+wget https://raw.githubusercontent.com/mitchamador/rt-n56u/master/rt-n56u-mt7621_cpufreq.sh
+wget https://raw.githubusercontent.com/mitchamador/rt-n56u/master/rt-n56u-mt7621_set_cpufreq.sh
+chmod +x rt-n56u-mt7621_cpufreq.sh
+chmod +x rt-n56u-mt7621_set_cpufreq.sh
+sh rt-n56u-mt7621_cpufreq.sh
+sh rt-n56u-mt7621_set_cpufreq.sh 1100
--- prbuild.sh
+++
@@ -1,4 +0,0 @@
-wget -q https://raw.githubusercontent.com/mitchamador/rt-n56u/master/rt-n56u-mt7621_set_cpufreq.sh -O - | bash -s -- 1100
-wget -q https://raw.githubusercontent.com/mitchamador/rt-n56u/master/rt-n56u-mt7621_cpufreq.sh -O - | bash
-./clear_tree.sh
-./build_firmware.sh
