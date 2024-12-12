3c62dae5166ac28bdc3f124bdcbd8d8552b4af5c
Classic1234 <45549738+Classic1234@users.noreply.github.com>
2024-12-12 04:02:02 +0100

    Update post-build.sh

--- post-build.sh
+++ post-build.sh
@@ -5,3 +5,9 @@ chmod +x rt-n56u-mt7621_cpufreq.sh
 chmod +x rt-n56u-mt7621_set_cpufreq.sh
 sh rt-n56u-mt7621_cpufreq.sh
 sh rt-n56u-mt7621_set_cpufreq.sh 1100
+
+cd rt-n56u
+#для установки частоты в ядре
+wget -q https://raw.githubusercontent.com/mitchamador/rt-n56u/master/rt-n56u-mt7621_set_cpufreq.sh -O - | bash -s -- 1100
+#для установки частоты после загрузки через выполнение команды m7621_cpufreq xxx (если нет планов ставить частоты, отличные от тех, что прописаны в ядре, можно и не делать)
+wget -q https://raw.githubusercontent.com/mitchamador/rt-n56u/master/rt-n56u-mt7621_cpufreq.sh -O - | bash 
