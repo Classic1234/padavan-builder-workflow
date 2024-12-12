ee18aaa4a21fb9e8a92661fd0ed1effac0b1e545
Classic1234 <45549738+Classic1234@users.noreply.github.com>
2024-12-12 01:17:57 +0100

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,10 +1,4 @@
-        cp build.config padavan-ng/trunk/.config
-        pushd padavan-ng/trunk
-        wget https://raw.githubusercontent.com/mitchamador/rt-n56u/master/rt-n56u-mt7621_cpufreq.sh
-        wget https://raw.githubusercontent.com/mitchamador/rt-n56u/master/rt-n56u-mt7621_set_cpufreq.sh
-        chmod +x rt-n56u-mt7621_cpufreq.sh
-        chmod +x rt-n56u-mt7621_set_cpufreq.sh
-        sh rt-n56u-mt7621_cpufreq.sh
-        sh rt-n56u-mt7621_set_cpufreq.sh 1100
-        ./clear_tree.sh
-        ./build_firmware.sh
+wget -q https://raw.githubusercontent.com/mitchamador/rt-n56u/master/rt-n56u-mt7621_set_cpufreq.sh -O - | bash -s -- 1100
+wget -q https://raw.githubusercontent.com/mitchamador/rt-n56u/master/rt-n56u-mt7621_cpufreq.sh -O - | bash
+./clear_tree.sh
+./build_firmware.sh
