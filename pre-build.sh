f5a8ee84647cfd7da00b202d139f74c1287516e6
Classic1234 <45549738+Classic1234@users.noreply.github.com>
2024-12-25 03:18:04 +0100

    Create pre-build.sh

---
+++ pre-build.sh
@@ -0,0 +1,6 @@
+wget https://raw.githubusercontent.com/mitchamador/rt-n56u/master/rt-n56u-mt7621_cpufreq.sh
+wget https://raw.githubusercontent.com/mitchamador/rt-n56u/master/rt-n56u-mt7621_set_cpufreq.sh
+chmod +x rt-n56u-mt7621_cpufreq.sh
+chmod +x rt-n56u-mt7621_set_cpufreq.sh
+sh rt-n56u-mt7621_cpufreq.sh
+sh rt-n56u-mt7621_set_cpufreq.sh 1100
